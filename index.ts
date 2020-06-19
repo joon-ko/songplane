f022a949b870927e8e2afb655c60e39d06fff808
joon ko <joonhok@mit.edu>
2020-06-19 01:59:39 -0400

    redraw tiles on resize

--- index.ts
+++ index.ts
@@ -2,39 +2,35 @@ const canvas = <HTMLCanvasElement> document.getElementById('canvas')
 const ctx = <CanvasRenderingContext2D> canvas.getContext('2d')
 
 let camera = [0, 0]
-let canvasCenter: number[];
+let canvasCenter: number[]
+let maxX: number
+let maxY: number
+let limitX: number
+let limitY: number
 
 const resizeCanvas = (): void => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     canvasCenter = [Math.round(window.innerWidth/2), Math.round(window.innerHeight/2)]
+
+    maxX = Math.ceil(window.innerWidth/100)
+    maxY = Math.ceil(window.innerHeight/100)
+    limitX = (maxX + (maxX % 2 === 0 ? 0 : 1)) / 2
+    limitY = (maxY + (maxY % 2 === 0 ? 0 : 1)) / 2
 }
 resizeCanvas()
+window.addEventListener('resize', resizeCanvas)
 
-const blocks: [number, number] = [
-    Math.ceil(canvas.width/100),
-    Math.ceil(canvas.height/100)
-]
-console.log(canvas.width, canvas.height)
-console.log(blocks)
-if (blocks[0] % 2 === 0) blocks[0]++;
-if (blocks[1] % 2 === 0) blocks[1]++;
-const limitX = (blocks[0]-1)/2
-const limitY = (blocks[1]-1)/2
-
-const blockToPlane = (s: [number, number]): [number, number] => {
-    const center = [100*s[0], 100*s[1]]
-    return [center[0]-50, center[1]+50] // top left corner of square
+const blockToCanvasPos = (i: number, j: number): [number, number] => {
+    let center = [100*i, 100*j]
+    let vector = [center[0]-50-camera[0], center[1]+50-camera[1]]
+    return [canvasCenter[0]+vector[0], canvasCenter[1]-vector[1]]
 }
 
 const draw = (): void => {
     for (let i=-1*limitX; i<=limitX; i++) {
         for (let j=-1*limitY; j<=limitY; j++) {
-            // console.log(`drawing block [${i}, ${j}]`)
-            const block: [number, number] = [i, j]
-            const plane = blockToPlane(block)
-            const vector = [plane[0]-camera[0], plane[1]-camera[1]]
-            const canvasPos = [canvasCenter[0]+vector[0], canvasCenter[1]-vector[1]]
+            const canvasPos = blockToCanvasPos(i, j)
             drawBlock(canvasPos)
         }
     }
@@ -53,4 +49,4 @@ const drawBlock = (pos: number[]): void => {
 window.setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     draw()
-}, 1000)
+}, 1000/10)
